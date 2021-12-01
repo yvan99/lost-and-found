abb1f1a65eeb86f63800caa34afe3c8464628038
yvan99 <ishimweyvan90@gmail.com>
2021-12-01 17:56:44 +0200

    signup channges annd many

--- login.php
+++ login.php
@@ -1,87 +1,87 @@
 <?php require 'inc/css.php' ?>
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'].'/lost-and-found/server/core/init.php';
-if(isset($_POST['login'])){
-	
-	$email=$_POST['email'];
-	$password=$_POST['password'];
+require_once $_SERVER['DOCUMENT_ROOT'] . '/lost-and-found/server/core/init.php';
+if (isset($_POST['login'])) {
 
-
-$client = new client();
-
-$result=$client->signin($email,$password);
-
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	if (empty($email) || empty($password)) {
+		$message = '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <strong> Empty fields found ,check your form </strong> <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">×</span>
+        </button>
+    </div>';
+	} else {
+		$client = new client();
+		$result = $client->signin($email, $password);
+	}
 }
 ?>
+
 <body id="bg">
-<div class="page-wraper">
-<div id="loading-area"></div>
-    <!-- Content -->
-    <div class="page-content bg-white login-style2" style="background-image: url(assets/homepage/images/background/bg6.jpg); background-size: cover;">
-        <div class="section-full">
-            <!-- Login Page -->
-            <div class="container">
-                <div class="row">
-					<div class="col-lg-6 col-md-6 d-flex">
-						<div class="text-white max-w400 align-self-center">
-							<div class="logo">
-								<a href="index.html"><img src="assets/homepage/images/logo-white2.png" alt=""></a>
+	<div class="page-wraper" >
+		<div id="loading-area"></div>
+		<!-- Content -->
+		<div class="page-content bg-white login-style2" style="background-image: url(assets/homepage/images/background/bg6.jpg); background-size: cover;">
+			<div class="section-full">
+				<!-- Login Page -->
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-6 col-md-6 d-flex">
+							<div class="text-white max-w400 align-self-center">
+
+								<h2 class="m-b10">Login To You Now</h2>
+								<p>paragraph content here</p>
+								
 							</div>
-							<h2 class="m-b10">Login To You Now</h2>
-							<p class="m-b40">Lorem Ipsum is simply dummy text of the printing and typesetting industry has been the industry.</p>
-							<ul class="list-inline m-a0">
-								<li><a target="_blank" href="https://www.facebook.com/" class="site-button white facebook circle "><i class="fa fa-facebook"></i></a></li>
-								<li><a target="_blank" href="https://www.linkedin.com/" class="site-button white linkedin circle "><i class="fa fa-linkedin"></i></a></li>
-								<li><a target="_blank" href="https://www.instagram.com/" class="site-button white instagram circle "><i class="fa fa-instagram"></i></a></li>
-								<li><a target="_blank" href="https://twitter.com/" class="site-button white twitter circle "><i class="fa fa-twitter"></i></a></li>
-							</ul>
 						</div>
-					</div>
-					<div class="col-lg-6 col-md-6">
-						<div class="login-2 submit-resume p-a30 seth">
-							<div class="tab-content nav">
-								<form id="login" class="tab-pane active col-12 p-a0 " method="POST">
-									<p class="font-weight-600">If you have an account with us, please log in.</p>
-									<div class="form-group">
-										<?php echo @$result;?>
-										<label>E-Mail Address*</label>
-										<div class="input-group">
-											<input name="email" required="" class="form-control" placeholder="Your Email Address" type="email">
+						<div class="col-lg-6 col-md-6">
+							<div class="login-2 submit-resume p-a30 seth">
+								<div class="tab-content nav">
+									<?php echo @$message; ?>
+									<form id="login" class="tab-pane active col-12 p-a0 " method="POST">
+										<p class="font-weight-600">If you have an account with us, please log in.</p>
+										<div class="form-group">
+											<?php echo @$result; ?>
+											<label>E-Mail Address*</label>
+											<div class="input-group">
+												<input name="email"  class="form-control" placeholder="Your Email Address" type="email">
+											</div>
 										</div>
-									</div>
-									<div class="form-group">
-										<label>Password *</label>
-										<div class="input-group">
-											<input name="password" required="" class="form-control " placeholder="Type Password" type="password">
+										<div class="form-group">
+											<label>Password *</label>
+											<div class="input-group">
+												<input name="password"  class="form-control " placeholder="Type Password" type="password">
+											</div>
 										</div>
-									</div>
-									<div class="text-center">
-										<button class="site-button float-left"name="login">login</button>
-										<a data-toggle="tab" href="#forgot-password" class="site-button-link forget-pass m-t15 float-right"><i class="fa fa-unlock-alt"></i> Forgot Password</a> 
-									</div>
-								</form>
-								<form id="forgot-password" class="tab-pane fade  col-12 p-a0">
-									<p>We will send you an email to reset your password. </p>
-									<div class="form-group">
-										<label>E-Mail address *</label>
-										<div class="input-group">
-											<input name="dzName" required="" class="form-control" placeholder="Your Email Address" type="email">
+										<div class="text-center">
+											<button class="site-button float-left" name="login">login</button>
+											<a class="btn btn-secondary text-dark loat-left" href="./">Back home</a>
+											<!-- <a data-toggle="tab" href="#forgot-password" class="site-button-link forget-pass m-t15 float-right"><i class="fa fa-unlock-alt"></i> Forgot Password</a> -->
 										</div>
-									</div>
-									<div class="text-left"> 
-										<a class="site-button outline gray" data-toggle="tab" href="#login">Back</a>
-										<button class="site-button pull-right">Submit</button>
-									</div>
-								</form>
+									</form>
+									<form id="forgot-password" class="tab-pane fade  col-12 p-a0">
+										<p>We will send you an email to reset your password. </p>
+										<div class="form-group">
+											<label>E-Mail address *</label>
+											<div class="input-group">
+												<input name="dzName"  class="form-control" placeholder="Your Email Address" type="email">
+											</div>
+										</div>
+										<div class="text-left">
+											<a class="site-button outline gray" data-toggle="tab" href="#login">Back</a>
+											<button class="site-button pull-right">Submit</button>
+										</div>
+									</form>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
+				<!-- Login Page END -->
 			</div>
-			<!-- Login Page END -->
 		</div>
+		<!-- Content END -->
+		<button class="scroltop fa fa-chevron-up"></button>
 	</div>
-    <!-- Content END -->
-    <button class="scroltop fa fa-chevron-up" ></button>
-</div>
-<?php require 'inc/js.php' ?>
+	<?php require 'inc/js.php' ?>
---
+++ reportlost.php
@@ -0,0 +1,108 @@
+<?php require 'inc/css.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/lost-and-found/server/core/init.php';
+?>
+
+<body id="bg">
+    <div id="loading-area"></div>
+    <div class="page-wraper">
+        <?php require 'inc/header.php' ?>
+
+        <div class="section-full content-inner-2 bg-white browse-job">
+            <div class="container">
+                <ul class="nav nav-tabs nav-tabs-1" id="myTab" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link site-button active" id="tabPersonal" data-toggle="tab" href="#personal" role="tab" aria-controls="personal" aria-selected="true">Report lost document</a>
+                    </li>
+                </ul>
+                <div class="tab-content" id="myTabContent">
+                    <div class="tab-pane fade show active" id="personal" role="tabpanel" aria-labelledby="tabPersonal">
+                        <div class="job-bx">
+                            <div class="row">
+                                <div class="col-lg-8">
+                                    <form class="job-alert-bx">
+                                        <div class="row">
+                                            <div class="col-6">
+                                                <div class="form-group">
+                                                    <label>Document Name</label>
+                                                    <input class="form-control" type="text" name="docName">
+                                                </div>
+                                            </div>
+                                            <div class="col-6">
+                                                <div class="form-group">
+                                                    <label>Document ID</label>
+                                                    <input class="form-control" type="text" name="docId">
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-6">
+                                                <div class="form-group">
+                                                    <label>Document type</label>
+
+                                                    <select name="docType">
+                                                        <option selected disabled>Select document type</option>
+                                                        <?php $sel = select('*', 'document_type', '1');
+                                                        foreach ($sel as $cate) :
+                                                        ?>
+                                                            <option value="<?php echo  $cate['doctype_id'] ?>"><?php echo  $cate['doctype_name'] ?></option>
+                                                        <?php endforeach ?>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="col-6">
+                                                <div class="form-group">
+                                                    <label>Where did you lost your document ?</label>
+                                                    <input class="form-control" type="text" name="docAddress">
+                                                </div>
+                                            </div>
+                                            <div class="col-6">
+                                                <div class="form-group">
+                                                    <label>When did you lost document</label>
+                                                    <input class="form-control" placeholder="DAY/MONTH/YEAR" type="text" name="docDate">
+                                                </div>
+                                            </div>
+                                       
+                                            <div class="col-lg-12">
+                                                <div class="form-group">
+                                                    <div class="custom-control custom-checkbox">
+                                                        <input type="checkbox" class="custom-control-input" id="job-alert-check" name="docAgree">
+                                                        <label class="custom-control-label" for="job-alert-check">I agree to the Terms and Conditions and Privacy Policy</label>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-12 text-center">
+                                                <button class="site-button"  type="submit" name="reportLosted">Report Now</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div class="col-lg-4 bg-gray">
+                                    <div class="p-a25">
+                                        <h6>Why should you create a job alert</h6>
+                                        <ul class="list-check primary">
+                                            <li>Get relevant jobs directly in your inbox</li>
+                                            <li>Stay updated with latest opportunities</li>
+                                            <li>Be the first one to apply</li>
+                                            <li>Create up to 5 personalized job alerts</li>
+                                        </ul>
+                                        <div class="dez-divider bg-gray-dark"></div>
+                                        <h6 class="font-14">Why <a href="../cdn-cgi/l/email-protection.html" class="__cf_email__" data-cfemail="aec7c0c8c1eecbd6cfc3dec2cb80cdc1c3">[email&#160;protected]</a></h6>
+                                        <p class="m-b10"><strong class="text-black m-r10">800,000+ </strong> Jobs</p>
+                                        <p class="m-b10"><strong class="text-black m-r10">100,000+</strong> CV searches daily</p>
+                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry has been the industry.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <!-- Create Account END -->
+        <?php require 'inc/footer.php' ?>
+    <!-- scroll top button -->
+    <button class="scroltop fa fa-arrow-up" ></button>
+</div>
+        <?php require 'inc/js.php';
